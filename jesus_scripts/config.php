3a11d7bfa28f727d2f9a3022d7ed711e72d5dfc8
Will Black <wrblack862@gmail.com>
2016-04-28 22:39:14 -0400

    initial commit

--- jesus_scripts/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $DB_USER = 'jmarque3990';
-    $DB_PASSWORD = 'jesus3990';
-    $DB_DATABASE = 'cpsc399LoginApp';
-    $DB_SERVER = 'mysql1.cs.clemson.edu';
-    $connection = new mysqli($DB_SERVER, $DB_USER, $DB_PASSWORD, $DB_DATABASE);
-
-?>
